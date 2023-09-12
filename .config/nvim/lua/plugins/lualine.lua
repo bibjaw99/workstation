c9ddaedec62322fc2abf923a81c93f48b572d3fd
bibjaw99 <bibjaw99@gmail.com>
2023-09-13 01:39:00 +0600

    lualine separators added

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -9,8 +9,8 @@ LuaLine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "gruvbox-material",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
