45be623763a012fe4cc7ffaa686f7ce2dc34d638
bibjaw99 <bibjaw99@gmail.com>
2023-09-14 13:28:02 +0600

    gurvbox-material transparent

--- .config/nvim/lua/habib/plugins/colorscheme.lua
+++ .config/nvim/lua/habib/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-	"sainnhe/gruvbox-material",
-	priority = 1000,
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd("let g:gruvbox_material_background= 'hard'")
-		vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
-	end,
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.o.background = "dark" -- or "light" for light mode
+    vim.cmd("let g:gruvbox_material_background= 'hard'")
+    vim.cmd("let g:gruvbox_material_transparent_background=2")
+    vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
+  end,
 }
--- .config/nvim/lua/habib/plugins/utils.lua
+++ .config/nvim/lua/habib/plugins/utils.lua
@@ -1,26 +1,21 @@
 return {
-	-- emmet
-	{
-		"mattn/emmet-vim",
-	},
-	-- lorem ipsum
-	{
-		"derektata/lorem.nvim",
-		config = function()
-			local lorem = require("lorem")
-			lorem.setup({
-				sentenceLength = "mixedShort",
-				comma = 1,
-			})
-		end,
-	},
-	-- Transparent nvim
-	{
-		"xiyaowong/transparent.nvim",
-	},
-
-	-- rainbow_csv
-	{
-		"mechatroner/rainbow_csv",
-	},
+  -- emmet
+  {
+    "mattn/emmet-vim",
+  },
+  -- lorem ipsum
+  {
+    "derektata/lorem.nvim",
+    config = function()
+      local lorem = require("lorem")
+      lorem.setup({
+        sentenceLength = "mixedShort",
+        comma = 1,
+      })
+    end,
+  },
+  -- rainbow_csv
+  {
+    "mechatroner/rainbow_csv",
+  },
 }
