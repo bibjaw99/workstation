4984dfa7cebace8f5e0b499686567604d961a281
bibjaw99 <bibjaw99@gmail.com>
2023-09-17 11:10:17 +0600

    bufferline modified

--- .config/nvim/lua/habib/plugins/bufferline.lua
+++ .config/nvim/lua/habib/plugins/bufferline.lua
@@ -4,6 +4,28 @@ return {
 	config = function()
 		local bufferline = require("bufferline")
 
-		bufferline.setup()
+		bufferline.setup({
+			options = {
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Tree",
+						highlight = "Directory",
+						separator = true, -- use a "true" to enable the default, or set your own character
+					},
+				},
+				diagnostics = "nvim_lsp",
+				separator_style = { "", "" },
+				modified_icon = "●",
+				show_close_icon = false,
+				show_buffer_close_icons = false,
+			},
+			highlights = {
+				fill = {
+					fg = "#ebdbb2",
+					bg = "#1d2021",
+				},
+			},
+		})
 	end,
 }
