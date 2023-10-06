1559a282d6627f999b54a8aa8a0287680a95b0ce
bibjaw99 <bibjaw99@gmail.com>
2023-10-06 21:23:08 +0600

    indent blankline version3 updated

--- .config/nvim/lua/habib/plugins/indent.lua
+++ .config/nvim/lua/habib/plugins/indent.lua
@@ -1,108 +1,41 @@
 return {
 	-- indent guides for Neovim
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			local indent_blankline = require("indent_blankline")
+	"lukas-reineke/indent-blankline.nvim",
+	config = function()
+		local highlight = {
+			"RainbowRed",
+			"RainbowYellow",
+			"RainbowBlue",
+			"RainbowTeal",
+			"RainbowGreen",
+			"RainbowOrange",
+			"RainbowCyan",
+		}
 
-			-- Dfining Colors
-			vim.opt.termguicolors = true
-			vim.cmd([[highlight IndentBlanklineIndent1 guifg=#458588 gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent2 guifg=#fe8019 gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent3 guifg=#b16286 gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent4 guifg=#689d6a gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent5 guifg=#d79921 gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent6 guifg=#b8bb26 gui=nocombine]])
-			vim.cmd([[highlight IndentBlanklineIndent7 guifg=#fb4934 gui=nocombine]])
+		local hooks = require("ibl.hooks")
+		-- create the highlight groups in the highlight setup hook, so they are reset
+		-- every time the colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
+			vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
+		end)
 
-			indent_blankline.setup({
-				opts = {
-					show_trailing_blankline_indent = false,
-				},
-				filetype_exclude = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"lazy",
-					"lsp-installer",
-					"termianl",
-					"NvimTree",
-					"mason",
-				},
-
-				buftype_exclude = { "terminal" },
-				space_char_blankline = " ",
-				show_current_context = false,
-				show_current_context_start = false,
-				context_patterns = {
-					"class",
-					"return",
-					"function",
-					"method",
-					"^if",
-					"^while",
-					"jsx_element",
-					"^for",
-					"^object",
-					"^table",
-					"block",
-					"arguments",
-					"if_statement",
-					"else_clause",
-					"jsx_element",
-					"jsx_self_closing_element",
-					"try_statement",
-					"catch_clause",
-					"import_statement",
-					"operation_type",
-				},
-				char_highlight_list = {
-					"IndentBlanklineIndent1",
-					"IndentBlanklineIndent2",
-					"IndentBlanklineIndent3",
-					"IndentBlanklineIndent4",
-					"IndentBlanklineIndent5",
-					"IndentBlanklineIndent6",
-				},
-				max_indent_increase = 1,
-				strict_tabs = true,
-				vim.opt.listchars:append("space: "),
-			})
-		end,
-	},
-
-	-- Active indent guide and indent text objects. When you're browsing
-	-- code, this highlights the current level of indentation, and animates
-	-- the highlighting.
-	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
+		local ibl = require("ibl")
+		ibl.setup({
+			indent = {
+				char = "│",
+				tab_char = "│",
+				highlight = highlight,
+			},
+			scope = {
+				show_start = true,
+				show_end = true,
+			},
+		})
+	end,
 }
