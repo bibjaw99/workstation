390e8817161b6da5504f9a35fbbe931852845119
bibjaw99 <bibjaw99@gmail.com>
2023-10-15 13:38:40 +0600

    organized importing dirs in lazy.lua

--- .config/nvim/lua/habib/core/lazy.lua
+++ .config/nvim/lua/habib/core/lazy.lua
@@ -1,27 +1,35 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local installed, lazy = pcall(require, "lazy")
 if not installed then
-	return
+  return
 end
 
-lazy.setup({ { import = "habib.plugins" }, { import = "habib.plugins.lsp" } }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-})
+lazy.setup(
+-- importing directories
+  {
+    { import = "habib.plugins" },
+    { import = "habib.plugins.lsp" },
+  },
+  -- end of importing dirs
+  {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  }
+)
