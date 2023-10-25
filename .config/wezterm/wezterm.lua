dc64facbeb3e742f26f105cedb53b3c56dac6f97
bibjaw99 <bibjaw99@gmail.com>
2023-10-25 19:15:52 +0600

    wezterm config-2

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,15 +5,34 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 -- =======================================================================
 config.color_scheme = "custom"
 config.window_background_opacity = 0.7
 
 -- =======================================================================
-config.font = wezterm.font("VictorMono Nerd Font Mono")
+config.font = wezterm.font("VictorMono Nerd Font Mono", { weight = "DemiBold", italic = false })
 config.font_size = 13
+config.font_rules = {
+  -- Bold
+  {
+    intensity = "Bold",
+    italic = false,
+    font = wezterm.font("VictorMono Nerd Font Mono", {
+      weight = "Bold",
+      style = "Normal",
+    }),
+  },
+  -- Italic
+  {
+    intensity = "Bold",
+    italic = true,
+    font = wezterm.font("VictorMono Nerd Font Mono", {
+      weight = "Bold",
+    }),
+  },
+}
 
 -- =======================================================================
 config.enable_tab_bar = false
