d98d7fcb8446711d15542ce4c5711e823c76c938
Ahsan Habib <bibjaw99@gmail.com>
2023-12-20 12:45:31 +0600

    Bold and Italic enabled nvim gruvbox colorscheme

--- .config/nvim/lua/habib/plugins/colorscheme.lua
+++ .config/nvim/lua/habib/plugins/colorscheme.lua
@@ -1,11 +1,15 @@
 return {
-	"sainnhe/gruvbox-material",
-	priority = 1000,
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd("let g:gruvbox_material_background= 'hard'")
-		vim.cmd("let g:gruvbox_material_transparent_background=2")
-		vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
-		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-	end,
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.o.background = "dark" -- or "light" for light mode
+    vim.cmd("let g:gruvbox_material_background= 'hard'")
+    vim.cmd("let g:gruvbox_material_transparent_background=2")
+    vim.cmd("let g:gruvbox_material_diagnostic_line_highlight=1")
+    vim.cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
+    vim.cmd("let g:gruvbox_material_enable_bold=1")
+    vim.cmd("let g:gruvbox_material_enable_italic=1")
+    vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
+    vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+  end,
 }
