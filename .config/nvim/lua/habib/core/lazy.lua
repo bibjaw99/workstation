cf9ca9568414f2764a4a88aacaf3e7c2381175de
Ahsan Habib <bibjaw99@gmail.com>
2023-12-21 23:39:40 +0600

    lazy spec

--- .config/nvim/lua/habib/core/lazy.lua
+++ .config/nvim/lua/habib/core/lazy.lua
@@ -1,46 +1,42 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local installed, lazy = pcall(require, "lazy")
 if not installed then
-  return
+	return
 end
 
-lazy.setup(
--- importing directories
-  {
-    { import = "habib.plugins" },
-    { import = "habib.plugins.lsp" },
-  },
+lazy.setup({
+	-- importing directories
+	spec = {
+		{ import = "habib.plugins" },
+		{ import = "habib.plugins.lsp" },
+	},
 
-  -- ui config
-  {
-    ui = {
-      border = "double",
-      size = {
-        width = 0.8,
-        height = 0.8,
-      },
-    },
-  },
+	-- ui config
+	ui = {
+		border = "double",
+		size = {
+			width = 0.8,
+			height = 0.8,
+		},
+	},
 
-  {
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    change_detection = {
-      notify = false,
-    },
-  }
-)
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
