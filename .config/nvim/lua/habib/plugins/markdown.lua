cd865b87963b067609046480242c21adaa7d7c87
Ahsan Habib <bibjaw99@gmail.com>
2024-01-09 09:39:43 +0600

    vim markdown plugin added

--- .config/nvim/lua/habib/plugins/markdown.lua
+++ .config/nvim/lua/habib/plugins/markdown.lua
@@ -1,11 +1,23 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-		vim.cmd("let g:mkdp_auto_start = 0")
-		vim.cmd("let g:mkdp_refresh_slow = 0")
-	end,
-	ft = { "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.cmd("let g:mkdp_auto_start = 0")
+      vim.cmd("let g:mkdp_refresh_slow = 0")
+    end,
+    ft = { "markdown" },
+  },
+
+  {
+    "preservim/vim-markdown",
+    dependencies = "godlygeek/tabular",
+
+    config = function()
+      vim.cmd("let g:vim_markdown_folding_disabled = 1")
+      vim.cmd("let g:vim_markdown_folding_style_pythonic = 1")
+    end,
+  },
 }
