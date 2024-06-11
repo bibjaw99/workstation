37df9e93ac1bb9139131f1c1d6f261dea0e9fc49
Ahsan Habib <bibjaw99@gmail.com>
2024-06-11 21:04:49 +0600

    telescope ui 2

--- .config/nvim/lua/habib/plugins/telescope.lua
+++ .config/nvim/lua/habib/plugins/telescope.lua
@@ -16,11 +16,11 @@ return {
 			telescope.setup({
 				defaults = {
 					layout_config = {
-						width = 0.99,
-						height = 0.99,
+						width = 0.9,
+						height = 0.9,
 						prompt_position = "bottom",
 						preview_cutoff = 120,
-						preview_width = 80,
+						preview_width = 75,
 						horizontal = { mirror = false },
 						vertical = { mirror = false },
 					},
