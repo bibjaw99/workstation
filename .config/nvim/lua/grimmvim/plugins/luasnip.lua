c3f1ed5819e9945c91736cdac4e4ff8c36993e52
Ahsan Habib <bibjaw99@gmail.com>
2024-06-24 15:11:12 +0600

    started working on luasnip

---
+++ .config/nvim/lua/grimmvim/plugins/luasnip.lua
@@ -0,0 +1,56 @@
+return {
+	"L3MON4D3/LuaSnip",
+	config = function()
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local sn = ls.snippet_node
+		local isn = ls.indent_snippet_node
+		local t = ls.text_node
+		local i = ls.insert_node
+		local f = ls.function_node
+		local c = ls.choice_node
+		local d = ls.dynamic_node
+		local r = ls.restore_node
+		local events = require("luasnip.util.events")
+		local ai = require("luasnip.nodes.absolute_indexer")
+		local extras = require("luasnip.extras")
+		local l = extras.lambda
+		local rep = extras.rep
+		local p = extras.partial
+		local m = extras.match
+		local n = extras.nonempty
+		local dl = extras.dynamic_lambda
+		local fmt = require("luasnip.extras.fmt").fmt
+		local fmta = require("luasnip.extras.fmt").fmta
+		local conds = require("luasnip.extras.expand_conditions")
+		local postfix = require("luasnip.extras.postfix").postfix
+		local types = require("luasnip.util.types")
+		local parse = require("luasnip.util.parser").parse_snippet
+		local ms = ls.multi_snippet
+		local k = require("luasnip.nodes.key_indexer").new_key
+
+		-- keybindings
+
+		vim.keymap.set({ "i" }, "<C-S-k>", function()
+			ls.expand()
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-Tab>", function()
+			ls.jump(1)
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-S-Tab>", function()
+			ls.jump(-1)
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-c>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
+
+		-- #########################
+		-- custom snippets here
+		-- #########################
+	end,
+}
