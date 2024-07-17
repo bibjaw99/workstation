224dd385ba39a44cadcfcff1e092e55beab742dc
Ahsan Habib <bibjaw99@gmail.com>
2024-07-17 18:32:15 +0600

    auto save session diabled

--- .config/nvim/lua/grimmvim/plugins/session.lua
+++ .config/nvim/lua/grimmvim/plugins/session.lua
@@ -7,6 +7,7 @@ return {
 		local auto_session = require("auto-session")
 		auto_session.setup({
 			auto_restore_enabled = false,
+			auto_save_enabled = false,
 			auto_session_suppress_dirs = {
 				"/",
 				"~/",
