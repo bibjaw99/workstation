aa5eee5d93a928f718cc4097ba5eacc9ba79b1c3
Ahsan Habib <bibjaw99@gmail.com>
2024-12-07 14:02:03 +0600

    update fuzzel and powermenu configs

--- .config/custom_scripts/power.sh
+++ .config/custom_scripts/power.sh
@@ -7,7 +7,7 @@ else
     suspend="systemctl suspend && i3lock -i ~/.config/walls/VIM.png"
 fi
 
-# special method for loggin out of i3 :(
+# special method for loggin out :(
 if [ "$DESKTOP_SESSION" = "i3" ]; then
     logout="killall i3"
   elif [ "$DESKTOP_SESSION" = "hyprland" ]; then
@@ -18,7 +18,14 @@ if [ "$DESKTOP_SESSION" = "i3" ]; then
     logout="loginctl terminate-user $USER"
 fi
 
-# Present the power menu to the user
+# Present the power menu dependin od display server
+
+# if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+#     chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" |  fuzzel --dmenu -l 4 --width 10 --anchor top-left -p "menu : ")
+#   elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+#     chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/powermenu.rasi"')
+# fi
+
 chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/powermenu.rasi"')
 
 # Perform the action based on user choice
