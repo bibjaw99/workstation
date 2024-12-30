89bfaf044fd211fcdb75add710c829fa2489a147
Ahsan Habib <bibjaw99@gmail.com>
2024-12-30 13:09:54 +0600

    add event for lualine

--- .config/nvim/lua/grimmvim/plugins/lualine.lua
+++ .config/nvim/lua/grimmvim/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lualine = require("lualine")
 
