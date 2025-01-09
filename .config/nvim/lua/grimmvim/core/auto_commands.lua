b82678c796f3121a17ba8cd18627b2f5c3dd9b52
Ahsan Habib <bibjaw99@gmail.com>
2025-01-09 23:42:20 +0600

    autocmd for hiding cursor in Snacks Dashboard

--- .config/nvim/lua/grimmvim/core/auto_commands.lua
+++ .config/nvim/lua/grimmvim/core/auto_commands.lua
@@ -55,3 +55,25 @@ autocmd("FileType", {
 		vim.opt_local.formatoptions:remove({ "r", "o" })
 	end,
 })
+
+-- hide cursor in SnacksDashboardOpened
+autocmd("User", {
+	pattern = "SnacksDashboardOpened",
+	callback = function()
+		local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+		hl.blend = 100
+		vim.api.nvim_set_hl(0, "Cursor", hl)
+		vim.cmd("set guicursor+=a:Cursor/lCursor")
+	end,
+})
+-- unhide cursor in SnacksDashboardClosed
+autocmd("User", {
+	pattern = "SnacksDashboardClosed",
+	callback = function()
+		local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+		hl.blend = 0
+		vim.api.nvim_set_hl(0, "Cursor", hl)
+		-- vim.opt.guicursor.append("a:Cursor/lCursor")
+		vim.cmd("set guicursor+=a:Cursor/lCursor")
+	end,
+})
