d879a5156c4fbf31928e72fc5715eb1e9bd58916
Ahsan Habib <bibjaw99@gmail.com>
2025-01-17 07:15:18 +0600

    fix undotree typo error and resizing window problem

--- .config/nvim/lua/grimmvim/plugins/util/substitute.lua
+++ .config/nvim/lua/grimmvim/plugins/util/substitute.lua
@@ -2,9 +2,9 @@ return {
 	"gbprod/substitute.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		local map = vim.keymap.set
 		local substitute = require("substitute")
 		substitute.setup()
-		local map = vim.keymap.set
 		map("n", "s", substitute.operator, { desc = "Substitute with motion", noremap = true, silent = true })
 		map("n", "ss", substitute.line, { desc = "Substitute line", noremap = true, silent = true })
 		map("n", "S", substitute.eol, { desc = "Substitute to end of line", noremap = true, silent = true })
--- .config/nvim/lua/grimmvim/plugins/util/undotree.lua
+++ .config/nvim/lua/grimmvim/plugins/util/undotree.lua
@@ -1,10 +1,28 @@
 return {
 	"mbbill/undotree",
 	event = "VeryLazy",
-	cofig = function()
+	config = function()
 		local map = vim.keymap.set
-		local undotree = require("undotree")
-		undotree.setup()
+
+		-- reset the window size from the last time
+		local api = vim.api
+		local function setUndotreeWinSize()
+			local winList = api.nvim_list_wins()
+			for _, winHandle in ipairs(winList) do
+				if
+					api.nvim_win_is_valid(winHandle)
+					and api.nvim_buf_get_option(api.nvim_win_get_buf(winHandle), "filetype") == "undotree"
+				then
+					api.nvim_win_set_width(winHandle, vim.g.undotree_SplitWidth)
+				end
+			end
+		end
+
+		api.nvim_create_user_command("Ut", function()
+			api.nvim_cmd(api.nvim_parse_cmd("UndotreeToggle", {}), {})
+			setUndotreeWinSize()
+		end, { desc = "load undotree" })
+
 		-- keymaps
 		map("n", "<leader>uu", ":UndotreeToggle<cr>", { desc = "UndoTree Toggle", noremap = true, silent = true })
 		map("n", "<leader>uf", ":UndotreeFocus<cr>", { desc = "UndoTree Focus", noremap = true, silent = true })
