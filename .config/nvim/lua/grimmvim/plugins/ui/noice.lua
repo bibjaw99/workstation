ae96ce39cdf2e5c96a52fe7ff107a4b97e76939c
Ahsan Habib <bibjaw99@gmail.com>
2025-06-18 17:12:32 +0600

    fix noice for good pls

--- .config/nvim/lua/grimmvim/plugins/ui/noice.lua
+++ .config/nvim/lua/grimmvim/plugins/ui/noice.lua
@@ -1,36 +1,7 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
 	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-			},
-		},
-		routes = {
-			{
-				filter = {
-					any = {
-						{
-							event = { "notify", "msg_show" },
-							find = "No information available",
-						},
-						{
-							event = "msg_show",
-							kind = "",
-							find = "written",
-						},
-					},
-				},
-				opts = {
-					skip = true,
-				},
-			},
-		},
 		-- views options
 		views = {
 			notify = {
@@ -71,39 +42,56 @@ return {
 				scrollbar = false,
 			},
 		},
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			},
+			signature = {
+				enabled = false,
+			},
+			hover = {
+				enabled = false,
+			},
+		},
+		routes = {
+			{
+				filter = {
+					any = {
+						{
+							event = { "notify", "msg_show" },
+							find = "No information available",
+						},
+						{
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+					},
+				},
+				opts = {
+					skip = true,
+				},
+			},
+		},
 		presets = {
 			bottom_search = false,
 			command_palette = true,
 			long_message_to_split = true,
-			inc_rename = false,
+			lsp_doc_border = true,
 		},
 	},
-
-	-- keymaps
-	keys = {
-		{
-			mode = "n",
-			"<leader>nh",
-			":Noice history<cr>",
-			desc = "Noice History",
-		},
-		{
-			mode = "n",
-			"<leader>nl",
-			":Noice last<cr>",
-			desc = "Noice Last msg",
-		},
-		{
-			mode = "n",
-			"<leader>na",
-			":Noice all<cr>",
-			desc = "Noice all",
-		},
-		{
-			mode = "n",
-			"<leader>nd",
-			":Noice dismiss<cr>",
-			desc = "Noice Dismiss",
-		},
-	},
+	config = function(_, opts)
+		local map = vim.keymap.set
+		if vim.o.filetype == "lazy" then
+			vim.cmd([[messages clear]])
+		end
+		require("noice").setup(opts)
+		-- keymaps
+		map("n", "<leader>nh", ":Noice history<cr>", { desc = "History", noremap = true, silent = true })
+		map("n", "<leader>nl", ":Noice last<cr>", { desc = "Last Msg", noremap = true, silent = true })
+		map("n", "<leader>na", ":Noice all<cr>", { desc = "All Msg", noremap = true, silent = true })
+		map("n", "<leader>nd", ":Noice dismiss<cr>", { desc = "Dismiss", noremap = true, silent = true })
+		map("n", "<leader>np", ":Noice pick<cr>", { desc = "Pick", noremap = true, silent = true })
+	end,
 }
