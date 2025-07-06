2c72087cb82d132497144f08f2aa056eae4b9db6
Ahsan Habib <bibjaw99@gmail.com>
2025-07-06 15:41:43 +0600

    update hyprland config with new packages and update system_package list

--- arch_install/scripts/drivers.sh
+++ arch_install/scripts/drivers.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# for my amd setup
-
-# yay -Syy \
-# xf86-video-amdgpu \
-# xf86-video-ati \
-# amd-ucode \
-# amdvlk \
-# vulkan-radeon
-
-# for my intel laptop
-
 packages=(
 # xorg
+# xf86-video-amdgpu
+# xf86-video-ati
+# amd-ucode
+# amdvlk
+# vulkan-radeon
 intel-media-driver
 vulkan-intel
 intel-gmmlib
--- arch_install/scripts/system_packages.sh
+++ arch_install/scripts/system_packages.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 packages=(
-xorg-server
-sddm
-plymouth
-neovim
-tree-sitter-cli
-texlive-binextra
-fzf
-i3-wm
-i3lock
-polybar
-rofi-wayland
-rofi-emoji-git
+# common system packages
+zsh
+zsh-syntax-highlighting
+zsh-autosuggestions lshw
+libnotify
 ghostty
 pcmanfm
+xarchiver
 gvfs
 gvfs-mtp
 gvfs-gphoto2
@@ -23,28 +17,38 @@ jmtpfs
 ntfs-3g
 mate-polkit
 xdg-desktop-portal
-xdg-desktop-portal-gtk
 xdg-user-dirs
-lxappearance-gtk3
-ttf-dejavu
-ttf-roboto
-ttf-jetbrains-mono-nerd
-ttf-font-awesome
-noto-fonts-emoji
-pavucontrol
-pipewire
-pipewire-pulse
-pipewire-jack
-wireplumber
+networkmanager
 network-manager-applet
-xarchiver
 wget
 unzip
 unrar
-htop
 gnome-keyring
 gnome-disk-utility
 libgnome-keyring
+brightnessctl
+
+# xorg-packages
+lxappearance-gtk3
+xdg-desktop-portal-gtk
+xsel
+xorg-xrandr
+xdotool
+picom
+nitrogen
+
+# i3-wm
+i3-wm
+i3lock
+dunst
+polybar
+
+# dev packages
+tmux
+neovim
+tree-sitter-cli
+texlive-binextra
+fzf
 ripgrep
 lazygit
 tokei
@@ -61,55 +65,69 @@ uthash
 libconfig
 meson
 ninja
-xsel
-cmus
 python-pip
-xorg-xrandr
-xdotool
-devour-git
-picom
-tree
-yazi
-poppler
-ueberzugpp
 rust
+
+# utility
+imagemagick
+python-pillow
+rofi-wayland
+rofi-emoji-git
+plymouth
 rate-mirrors-bin
 pacman-contrib
-tmux
 eza
 starship
-dunst
-python-pillow
-imagemagick
-nitrogen
-feh
-mpv
 zathura
 zathura-pdf-poppler
-zsh
-zsh-syntax-highlighting
-zsh-autosuggestions lshw
+poppler
+
+# sound and media
+pavucontrol
+pipewire
+pipewire-pulse
+pipewire-jack
+pipewire-alsa
+wireplumber
 blueman
 bluez
 bluez-utils
 gnome-bluetooth
-brightnessctl
+cmus
+feh
+mpv
+
+# fonts and UI
+ttf-dejavu
+ttf-roboto
+ttf-jetbrains-mono-nerd
+ttf-font-awesome
+noto-fonts-emoji
 tela-circle-icon-theme-black
 materia-gtk-theme
-sway
-hyprland
+
+# wayland packages
+wayland
+xorg-xwayland
 waybar
+grim
+slurp
+nwg-look
+wl-clipboard
+
+# sway packages
+sway
 swaync
 swaybg
 swaylock
-libnotify
-grim
-slurp
 xdg-desktop-portal-wlr
+
+# hyprland packages
+hyprland
+hyprlock
+hyprpaper
 xdg-desktop-portal-hyprland
-nwg-look
-wl-clipboard
-wlsunset
+mako
 )
 
 for package in "${packages[@]}"; do
