9d428199811d9e5fa410703f0b24584f380ffdcd
Ahsan Habib <bibjaw99@gmail.com>
2025-08-08 16:21:04 +0600

    update i3 and polybar config

--- dotfiles/config/polybar_configs/polybar_floating/i3_bar.sh
+++ dotfiles/config/polybar_configs/polybar_floating/i3_bar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
