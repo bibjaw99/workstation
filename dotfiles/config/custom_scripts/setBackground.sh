196c8199652ffb9acb396ba1bd3986798621c5e0
Ahsan Habib <bibjaw99@gmail.com>
2025-08-30 13:59:56 +0600

    organize hyprland and swaywm configs

--- dotfiles/config/custom_scripts/setBackground.sh
+++ dotfiles/config/custom_scripts/setBackground.sh
@@ -12,16 +12,20 @@ for display_session in "${display_sessions[@]}"; do
     "wayland")
       if [[ -z "$bg" ]]; then
         swaybg -i "$default_bg" -m fill
+        exit 1
       else
         swaybg -i "$bgFolder$bg" -m fill
+        exit 1
       fi
       ;;
 
     "x11")
       if [[ -z "$bg" ]]; then
         feh --bg-fill "$default_bg"
+        exit 1
       else
         feh --bg-fill "$bgFolder$bg"
+        exit 1
       fi
       ;;
   esac
