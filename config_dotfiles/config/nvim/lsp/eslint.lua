cabd7006b495597f9f51f3a6f4f035faf4028fff
Ahsan Habib <bibjaw99@gmail.com>
2025-09-29 12:14:28 +0600

    update eslint lspconfig

--- config_dotfiles/config/nvim/lsp/eslint.lua
+++ config_dotfiles/config/nvim/lsp/eslint.lua
@@ -1,5 +1,6 @@
 return {
 	cmd = { "vscode-eslint-language-server", "--stdio" },
+	root_markers = { ".eslintrc", ".eslintrc.json" },
 	filetypes = {
 		"javascript",
 		"javascriptreact",
@@ -7,5 +8,37 @@ return {
 		"typescript",
 		"typescriptreact",
 		"typescript.tsx",
+	},
+	settings = {
+		codeAction = {
+			disableRuleComment = {
+				enable = true,
+				location = "separateLine",
+			},
+			showDocumentation = {
+				enable = true,
+			},
+		},
+		codeActionOnSave = {
+			enable = false,
+			mode = "all",
+		},
+		experimental = {
+			useFlatConfig = false,
+		},
+		format = true,
+		nodePath = "",
+		onIgnoredFiles = "off",
+		problems = {
+			shortenToSingleLine = false,
+		},
+		quiet = false,
+		rulesCustomizations = {},
+		run = "onType",
+		useESLintClass = false,
+		validate = "on",
+		workingDirectory = {
+			mode = "auto",
+		},
 	},
 }
